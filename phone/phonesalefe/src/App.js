7e151756ba0e644c9bf49f2860caaaae4a561430
Secret147 <chungnguyen2002xtc@gmail.com>
2023-07-10 16:30:13 +0700

    repairproject

---
+++ phone/phonesalefe/src/App.js
@@ -0,0 +1,12 @@
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
+
+function App() {
+    return (
+        <div className="App">
+            <p>REACTJS</p>
+        </div>
+    );
+}
+
+export default App;
