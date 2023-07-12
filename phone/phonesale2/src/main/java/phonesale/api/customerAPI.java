145cfc4897be7013b56d667dbf1eabbfef982696
Secret147 <chungnguyen2002xtc@gmail.com>
2023-07-12 23:48:27 +0700

    additional-12/07/2023

--- phone/phonesale2/src/main/java/phonesale/api/customerAPI.java
+++ phone/phonesale2/src/main/java/phonesale/api/customerAPI.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +14,16 @@ import phonesale.entity.customerEntity;
 import phonesale.repository.customerRepository;
 
 @RestController
+@CrossOrigin
 public class customerAPI {
 	@Autowired
 	private customerRepository customerRe;
 
-	@PostMapping("/new")
+	@PostMapping("/customer/new")
 	public ResponseEntity<?> addUser(@RequestBody customerEntity customer){
+		if(customerRe.findByName(customer.getName()) != null) {
+			return ResponseEntity.badRequest().body("tai khoan da ton tai");
+		}
 		customerRe.save(customer);
 		return ResponseEntity.ok(customer);
 		
--- phone/phonesale2/src/main/java/phonesale/repository/customerRepository.java
+++ phone/phonesale2/src/main/java/phonesale/repository/customerRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import phonesale.entity.customerEntity;
 
 public interface customerRepository extends JpaRepository<customerEntity, Long>{
+	Object findByName(String name);
 
 }
